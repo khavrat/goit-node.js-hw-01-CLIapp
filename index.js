d41487e783024eb19c20816aec4636cf4c1887f7
khavrat <ihavrat@gmail.com>
2023-05-04 14:16:16 +0300

    crated CLIapp

---
+++ index.js
@@ -0,0 +1,53 @@
+const contacts = require("./contacts");
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+console.log('argv :>> ', argv);
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+        const allContacts = await contacts.listContacts();
+        return console.table(allContacts);
+    case "get":
+        const oneContact = await contacts.getContactById(id);
+        return console.log(oneContact); 
+      case "add":
+          const newContact = await contacts.addContact({ name, email, phone });
+          return console.log(newContact); 
+      case "remove":
+          const removeContact = await contacts.removeContact(id);
+          return console.log(removeContact); 
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+const actionIndex = process.argv.indexOf("--action");
+console.log("actionIndex :>> ", actionIndex);
+    
+if (actionIndex !== -1) {
+    const action = process.argv[actionIndex + 1];
+    invokeAction({ action });
+}
+
+// invokeAction(argv);
+
+invokeAction({
+  action: argv.action,
+  id: argv.id,
+  name: argv.name,
+  email: argv.email,
+  phone: argv.phone,
+});
+
